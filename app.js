fc1d5b1bae3e2dda6b49a20e44b29086867c3a5b
Scott M Miller <smmiller.work@gmail.com>
2015-04-28 20:00:50 -0600

    complete?!?!

---
+++ app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+
+	var color = 'white';
+	var colors = 'white green red blue yellow';
+
+
+
+	$('.box').on('click', function() {
+		$(this).removeClass(colors);
+		$(this).addClass(color);
+	});
+
+	$('.box').on('dblclick', function() {
+		$(this).removeClass(colors);
+	});
+
+	$('#reset').on('click', function () {
+		$('.box').removeClass(colors);
+	});
+
+
+	$('#red').on('click', function() {
+		color = 'red';
+	});
+
+	$('#green').on('click', function() {
+		color = 'green';
+	});
+
+	$('#blue').on('click', function () {
+		color = 'blue';
+	});
+
+	$('#yellow').on('click', function () {
+		color = 'yellow';
+	});
+
+	$('#white').on('click', function () {
+		color = 'white';
+	});
+
+
+
+});
